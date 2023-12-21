4f6748db4236f51e6eb1b0711ddde49d35b725d1
Kartik9082 <kashyap.kartik2000@gmail.com>
2023-12-21 16:14:24 +0530

    Created Movies Recommendation list

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,10 +5,11 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className="bg-black">
-      <div className="-m-52 relative z-20">
+      <div className=" -mt-52 relative z-20">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Top Rated"} movies={movies.popularMovies} />
-        <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular Movies"} movies={movies.popularMovies} />
+        <MovieList title={"Top Rated Movies"} movies={movies.topRatedMovies} />
+        <MovieList title={"UpComing Movies"} movies={movies.upcomingMovies} />
       </div>
     </div>
   );
